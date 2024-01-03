0e2dddeff12c44adf65a6dc43911b9eceea71e86
Karsei <karseidev@gmail.com>
2024-01-03 09:27:48 +0900

    init commit

---
+++ src/test/java/kr/pe/karsei/tobyspringinflearn/TobyspringInflearnApplicationTests.java
@@ -0,0 +1,11 @@
+package kr.pe.karsei.tobyspringinflearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobyspringInflearnApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
