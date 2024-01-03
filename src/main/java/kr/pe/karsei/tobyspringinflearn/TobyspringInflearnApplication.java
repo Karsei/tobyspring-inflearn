54fd59642e86eef06522565906ec19035a137599
Karsei <karseidev@gmail.com>
2024-01-03 09:32:58 +0900

    feat: tomcat 서블릿 컨테이너 띄우기

--- src/main/java/kr/pe/karsei/tobyspringinflearn/TobyspringInflearnApplication.java
+++ src/main/java/kr/pe/karsei/tobyspringinflearn/TobyspringInflearnApplication.java
@@ -1,6 +1,16 @@
 package kr.pe.karsei.tobyspringinflearn;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
 public class TobyspringInflearnApplication {
     public static void main(String[] args) {
+        // Tomcat 뿐만 아니라 다른 서버로도 이용 가능하도록 스프링에서 추상화를 지원한다.
+        ServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        WebServer webServer = factory.getWebServer();
+
+        // 웹 서버 시작
+        webServer.start();
     }
 }
