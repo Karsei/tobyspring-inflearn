0a121ed5111db33188a95189f16464bdd926ffc3
Karsei <karseidev@gmail.com>
2024-01-03 17:42:17 +0900

    feat: 스프링 컨테이너 생성 후 빈으로 이용하도록 수정

--- src/main/java/kr/pe/karsei/tobyspringinflearn/TobyspringInflearnApplication.java
+++ src/main/java/kr/pe/karsei/tobyspringinflearn/TobyspringInflearnApplication.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -15,11 +16,15 @@ import java.io.IOException;
 
 public class TobyspringInflearnApplication {
     public static void main(String[] args) {
+        GenericApplicationContext context = new GenericApplicationContext();
+        // 메타 정보를 넣어주는 방식으로 Bean 등록 (보통 Bean 클래스를 지정해준다) Bean 이 어떻게 구성되어 지는가 등을
+        context.registerBean(HelloController.class);
+        // 컨테이너를 초기화 -> Bean 을 만들어줌
+        context.refresh();
+
         // Tomcat 뿐만 아니라 다른 서버로도 이용 가능하도록 스프링에서 추상화를 지원한다.
         ServletWebServerFactory factory = new TomcatServletWebServerFactory();
         WebServer webServer = factory.getWebServer(servletContext -> {
-            HelloController helloController = new HelloController();
-
             // 서블릿을 추가한다고 단순히 서블릿이 동작되지는 않는다. 맵핑을 추가해야 한다.
             servletContext
                     .addServlet("frontController", new HttpServlet() {
@@ -28,6 +33,7 @@ public class TobyspringInflearnApplication {
                             if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                                 String name = req.getParameter("name");
 
+                                HelloController helloController = context.getBean(HelloController.class);
                                 String returnedName = helloController.hello(name);
 
                                 // resp.setStatus(HttpStatus.OK.value()); // 에러가 나지 않는한 서블릿 컨테이너가 기본적으로 200 전달
