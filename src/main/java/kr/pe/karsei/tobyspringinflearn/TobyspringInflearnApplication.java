654fd97a03b113b4da80701352acc4b54e0a3bce
Karsei <karseidev@gmail.com>
2024-01-03 10:34:19 +0900

    feat: 프론트 컨트롤러로 전환

--- src/main/java/kr/pe/karsei/tobyspringinflearn/TobyspringInflearnApplication.java
+++ src/main/java/kr/pe/karsei/tobyspringinflearn/TobyspringInflearnApplication.java
@@ -1,6 +1,5 @@
 package kr.pe.karsei.tobyspringinflearn;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -8,6 +7,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -19,18 +19,27 @@ public class TobyspringInflearnApplication {
         ServletWebServerFactory factory = new TomcatServletWebServerFactory();
         WebServer webServer = factory.getWebServer(servletContext -> {
             // 서블릿을 추가한다고 단순히 서블릿이 동작되지는 않는다. 맵핑을 추가해야 한다.
-            servletContext.addServlet("hello", new HttpServlet() {
-                @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-                    String name = req.getParameter("name");
+            servletContext
+                    .addServlet("frontController", new HttpServlet() {
+                        @Override
+                        protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+                            if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                                String name = req.getParameter("name");
 
-                    resp.setStatus(HttpStatus.OK.value());
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                    resp.getWriter().println("Hello Servlet: %s".formatted(name));
-                }
-            })
+                                resp.setStatus(HttpStatus.OK.value());
+                                resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                                resp.getWriter().println("Hello Servlet: %s".formatted(name));
+                            }
+                            else if (req.getRequestURI().equals("/user")) {
+                                // Nothing
+                            }
+                            else {
+                                resp.setStatus(HttpStatus.NOT_FOUND.value());
+                            }
+                        }
+                    })
                     // 서블릿 컨테이너가 요청이 들어올 때 어느 서블릿으로 연결할지의 맵핑을 정해주어야 한다.
-                    .addMapping("/hello");
+                    .addMapping("/*");
         });
 
         // 웹 서버 시작
