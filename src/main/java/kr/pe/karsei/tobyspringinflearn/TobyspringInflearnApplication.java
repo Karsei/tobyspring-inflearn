849429f83490c2d56889f29bb800e2bf9d299b48
Karsei <karseidev@gmail.com>
2024-01-03 09:30:19 +0900

    del: 스프링 기반 시작 메서드 삭제

--- src/main/java/kr/pe/karsei/tobyspringinflearn/TobyspringInflearnApplication.java
+++ src/main/java/kr/pe/karsei/tobyspringinflearn/TobyspringInflearnApplication.java
@@ -1,11 +1,6 @@
 package kr.pe.karsei.tobyspringinflearn;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class TobyspringInflearnApplication {
     public static void main(String[] args) {
-        SpringApplication.run(TobyspringInflearnApplication.class, args);
     }
 }
