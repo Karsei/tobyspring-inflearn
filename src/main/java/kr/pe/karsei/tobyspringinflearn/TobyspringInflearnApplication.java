707b0a5ddc78ce54bf35e315e501b12fe1ec9f6b
Karsei <karseidev@gmail.com>
2024-01-08 13:09:13 +0900

    chore: 주석 추가

--- src/main/java/kr/pe/karsei/tobyspringinflearn/TobyspringInflearnApplication.java
+++ src/main/java/kr/pe/karsei/tobyspringinflearn/TobyspringInflearnApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.DispatcherServlet;
 @Configuration
 @ComponentScan // 이 클래스가 있는 패키지부터 시작해서 하위 패키지 찾아서 @Component 가 붙은 것을 찾아내고 알아서 등록해줌
 public class TobyspringInflearnApplication {
+    // 이거 없애면 작동 안 됨
     @Bean
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
@@ -23,5 +24,6 @@ public class TobyspringInflearnApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TobyspringInflearnApplication.class, args);
+        // MySpringApplication.run(TobyspringInflearnApplication.class, args);
     }
 }
