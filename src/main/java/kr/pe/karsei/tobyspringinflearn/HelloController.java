6555369841441ddb934c93bfe6514e7a5f82312b
Karsei <karseidev@gmail.com>
2024-01-10 12:07:34 +0900

    test: DI, 단위 테스트를 통한 테스트 작성

--- src/main/java/kr/pe/karsei/tobyspringinflearn/HelloController.java
+++ src/main/java/kr/pe/karsei/tobyspringinflearn/HelloController.java
@@ -1,29 +1,35 @@
 package kr.pe.karsei.tobyspringinflearn;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Objects;
 
 //@MyComponent // @Component 로도 가능
 //@Controller // Stereotype(전형적인) 컴포넌트
 @RestController // @Controller + @ResponseBody
 public class HelloController {
     private final HelloService service;
-    private final ApplicationContext applicationContext;
+//    private final ApplicationContext applicationContext;
 
-    public HelloController(HelloService service, ApplicationContext applicationContext) {
+    public HelloController(HelloService service) {
         this.service = service;
-        this.applicationContext = applicationContext;
-
-        System.out.println(applicationContext);
     }
+
+
+//    public HelloController(HelloService service,
+//                           ApplicationContext applicationContext
+//    ) {
+//        this.service = service;
+//        this.applicationContext = applicationContext;
+//
+//        System.out.println(applicationContext);
+//    }
 
     @GetMapping("hello")
     // 보통 String 으로 반환하면 해당 문자열의 view 를 찾게 된다.
     // 그대로 뿌려주려면 아래 Annotation 을 붙여야 한다.
     public String hello(String name) {
-        return service.sayHello(Objects.requireNonNull(name));
+        //return service.sayHello(Objects.requireNonNull(name));
+        if (name == null || name.trim().length() == 0) throw new IllegalArgumentException();
+        return service.sayHello(name);
     }
 }
