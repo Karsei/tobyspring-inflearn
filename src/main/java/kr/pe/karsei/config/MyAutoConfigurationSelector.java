b8bff0ace60d50a6e6fd69afa996714f1dd2867f
Karsei <karseidev@gmail.com>
2024-01-15 12:23:30 +0900

    feat: 자동 구성 정보 파일을 통해서 불러오도록 수정

--- src/main/java/kr/pe/karsei/config/MyAutoConfigurationSelector.java
+++ src/main/java/kr/pe/karsei/config/MyAutoConfigurationSelector.java
@@ -1,14 +1,30 @@
 package kr.pe.karsei.config;
 
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.stream.StreamSupport;
+
 public class MyAutoConfigurationSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    // BeanClassLoaderAware interface 또는 생성자 주입으로 사용
+    public MyAutoConfigurationSelector(ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-        return new String[] {
-                "kr.pe.karsei.config.autoconfig.DispatcherServletConfig",
-                "kr.pe.karsei.config.autoconfig.TomcatWebServerConfig",
-        };
+        // ImportCandidates.load 를 보면 META-INF/spring/ 안에서 불러온다고 나와 있음
+        Iterable<String> candidates = ImportCandidates.load(MyAutoConfiguration.class, classLoader);
+        return StreamSupport.stream(candidates.spliterator(), false)
+                .toArray(String[]::new);
+
+        // return new String[] {
+        //         "kr.pe.karsei.config.autoconfig.DispatcherServletConfig",
+        //         "kr.pe.karsei.config.autoconfig.TomcatWebServerConfig",
+        // };
     }
 }
