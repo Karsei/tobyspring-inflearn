b470e554ade6794090d500eb407914df0295437d
Karsei <karseidev@gmail.com>
2024-01-16 12:39:33 +0900

    feat: jetty 웹 서버 적용, Conditional 애노테이션 적용

--- src/main/java/kr/pe/karsei/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/kr/pe/karsei/config/autoconfig/TomcatWebServerConfig.java
@@ -3,14 +3,22 @@ package kr.pe.karsei.config.autoconfig;
 import kr.pe.karsei.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
 //@Configuration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class) // Condition 인터페이스를 참조함
 public class TomcatWebServerConfig {
-    @Bean
+    @Bean("tomcatWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
+    }
+
+    static class TomcatCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return false;
+        }
     }
 }
