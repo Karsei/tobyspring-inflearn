aec7305ea01b9fdbf3a65aca1f596e84a52fa9e8
Karsei <karseidev@gmail.com>
2024-01-18 12:38:27 +0900

    feat: ConditionalOnMissingBean 활용한 자동 구성 정보 대체

--- src/main/java/kr/pe/karsei/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/kr/pe/karsei/config/autoconfig/JettyWebServerConfig.java
@@ -2,6 +2,7 @@ package kr.pe.karsei.config.autoconfig;
 
 import kr.pe.karsei.config.ConditionalMyOnClass;
 import kr.pe.karsei.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -17,6 +18,7 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean("jettyWebServerFactory")
+    @ConditionalOnMissingBean // 사용자가 등록한 Bean 이 없으면 해당 Bean 을 등록한다.
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
     }
--- src/main/java/kr/pe/karsei/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/kr/pe/karsei/config/autoconfig/TomcatWebServerConfig.java
@@ -2,6 +2,7 @@ package kr.pe.karsei.config.autoconfig;
 
 import kr.pe.karsei.config.ConditionalMyOnClass;
 import kr.pe.karsei.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.*;
@@ -13,6 +14,7 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
     @Bean("tomcatWebServerFactory")
+    @ConditionalOnMissingBean // 사용자가 등록한 Bean 이 없으면 해당 Bean 을 등록한다.
     public ServletWebServerFactory servletWebServerFactory() {
         return new TomcatServletWebServerFactory();
     }
